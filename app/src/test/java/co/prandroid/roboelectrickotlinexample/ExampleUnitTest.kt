f53a0ff2ef2ea7dd334f6465ac7c86ae1481bc2a
dharmakshetri <dharma.kshetri@gmail.com>
2017-09-18 21:04:06 -0500

    added some other events...

--- app/src/test/java/co/prandroid/roboelectrickotlinexample/ExampleUnitTest.kt
+++ app/src/test/java/co/prandroid/roboelectrickotlinexample/ExampleUnitTest.kt
@@ -2,40 +2,65 @@ package co.prandroid.roboelectrickotlinexample
 
 import android.content.Intent
 import android.widget.Button
+import android.widget.TextView
 import co.prandroid.robolectricexample.MainActivity
 import co.prandroid.robolectricexample.SecondActivity
 import org.junit.Test
 
-import org.assertj.core.api.Assertions.assertThat
 
 
+import org.junit.Before
 
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric
-import org.robolectric.RobolectricTestRunner
+import org.robolectric.Shadows
 import org.robolectric.Shadows.shadowOf
+
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Assert
+import org.junit.Assert.*
+import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
-import org.junit.Assert.assertNotNull
+import org.robolectric.shadow.api.Shadow
+import org.robolectric.shadows.ShadowActivity
+import org.robolectric.shadows.ShadowIntent
+
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(RobolectricTestRunner::class)
-@Config(constants=BuildConfig::class)
+@Config(constants=BuildConfig::class, packageName = "co.prandroid.roboelectrickotlinexample")
+
 class ExampleUnitTest {
+    var mainActivity:MainActivity= null!!;
+    @Before
+    fun init(){
+        mainActivity = Robolectric.setupActivity(MainActivity::class.java)
+    }
 
     @Test
     fun clickingLoginButton_shouldStartSecondActivity(){
-        var mainActivity= Robolectric.setupActivity(MainActivity::class.java)
-
         val button= mainActivity.findViewById<Button>(R.id.button1)
+
         assertNotNull(button);
         button.performClick()
 
 
-        var intent= Intent(mainActivity, SecondActivity::class.java)
-        assertThat(shadowOf(mainActivity).getNextStartedActivity()).isEqualTo(intent);
+        val showActivity:ShadowActivity= Shadows.shadowOf(mainActivity)
+        val intent:Intent=showActivity.nextStartedActivity
+        val shadowIntent:ShadowIntent=shadowOf(intent)
+        assertThat(shadowIntent.intentClass.name, equalTo(MainActivity::class.java!!.getName()))
+    }
+
+    @Test
+    fun checkTextViewString_presentOrNot(){
+        val textView=mainActivity.findViewById<TextView>(R.id.textView)
+        val stringValue=textView.text.toString()
+        assertThat(stringValue,equalTo("Hello World!"))
     }
 
 }
